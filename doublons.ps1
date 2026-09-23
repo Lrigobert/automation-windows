0af812e47c65f968c585ccd92f1424ffa75b3d6f
Lrigobert <ldardaorigobert78@gmail.com>
2026-09-23 23:46:14 +0200

    Doublons : rapport dans Classement

---
+++ doublons.ps1
@@ -0,0 +1,29 @@
+$racine  = "$env:USERPROFILE\Documents\Classement"
+$rapport = Join-Path $racine 'rapport_doublons.csv'
+
+$fichiers = Get-ChildItem $racine -Recurse -File | Where-Object { $_.Name -notlike 'index*.csv' }
+
+$groupes = $fichiers | Get-FileHash -Algorithm SHA256 |
+  Group-Object Hash | Where-Object Count -gt 1
+
+$lignes = foreach ($g in $groupes) {
+  $tri = $g.Group | Sort-Object { $_.Path.Length }
+  $i = 0
+  foreach ($h in $tri) {
+    $i++
+    [pscustomobject]@{
+      Groupe = $g.Name.Substring(0, 8)
+      Statut = if ($i -eq 1) { 'A GARDER' } else { 'DOUBLON' }
+      Fichier = Split-Path $h.Path -Leaf
+      Chemin = $h.Path
+    }
+  }
+}
+
+if ($lignes) {
+  $lignes | Export-Csv $rapport -Delimiter ';' -NoTypeInformation -Encoding UTF8
+  $lignes | Format-Table Groupe, Statut, Fichier -AutoSize
+  Write-Host "Rapport : $rapport"
+} else {
+  Write-Host "Aucun doublon exact."
+}
